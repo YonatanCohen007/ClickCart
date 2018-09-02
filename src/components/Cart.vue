609bbc3b40ed4a21c1bf47f63274db0dd121db43
unknown <yec1997@gmail.com>
2018-09-02 15:56:36 +0300

    File (Cart.vue): Adding the Cart component

---
+++ src/components/Cart.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1 class="container title">Your Cart</h1>
+    <product v-for="product in selectedProducts" :product="product" :cart="true"></product>
+  </div>
+</template>
+
+<script>
+
+import {mapState} from 'vuex'
+import Product from './Product.vue'
+
+export default {
+  data () {
+    return {
+    }
+  },
+
+  computed: {
+    ...mapState(['selectedProducts'])
+  },
+
+  components: {
+    'product': Product
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
